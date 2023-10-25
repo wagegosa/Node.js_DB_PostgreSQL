f846989c6dafd3f66cd6703835d833ffe3df340a
wagegosa <wagegosa@gmail.com>
2023-10-25 17:29:47 -0500

    se crea conexion con la DB.

---
+++ libs/postgres.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+
+async function getConnection() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'root',
+    password: 'root',
+    database: 'my_store'
+  });
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
