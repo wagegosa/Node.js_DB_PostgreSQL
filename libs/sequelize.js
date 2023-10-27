1a2b48153f7ab90d0111bbb16f850cf20ef78dd9
wagegosa <wagegosa@gmail.com>
2023-10-27 09:35:33 -0500

    Se agrega los modelos y sincronizamos a Sequelize.

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const { config } = require('./../config/config');
+const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPass);
@@ -10,5 +11,9 @@ const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: console.log,
 });
+
+setupModels(sequelize);
+
+sequelize.sync();
 
 module.exports = sequelize;
