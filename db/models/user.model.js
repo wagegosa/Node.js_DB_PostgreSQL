c7095821052561ad36cfa31cf6983e3fa7f3f744
wagegosa <wagegosa@gmail.com>
2023-10-27 09:27:05 -0500

    Se crea model de usuarios.

---
+++ db/models/user.model.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes, Sequelize} = require('sequelize')
+
+const USER_TABLE = 'users';
+
+const userSchema = {
+    id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    email:{
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+    },
+    password:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    createdAt:{
+        allowNull: false,
+        type: DataTypes.DATE,
+        fiel: 'create_at',
+        defaultValue: Sequelize.NOW
+    }
+}
+
+class User extends Model{
+    static associate(){
+        // associate
+    }
+    static config(sequelize){
+        return {
+            sequelize,
+            tableName: USER_TABLE,
+            modelName: 'User',
+            timestamps: false
+        }
+    }
+}
+
+module.exports = { USER_TABLE, userSchema, User }
