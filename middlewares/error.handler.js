fae55112af347cecfe24d786248e7c95496a8f57
wagegosa <wagegosa@gmail.com>
2023-10-27 09:25:58 -0500

    Se agrega función para manejo de errores del ORM.

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,5 +1,6 @@
+const { ValidationError } = require("sequelize");
+
 function logErrors (err, req, res, next) {
-  console.error(err);
   next(err);
 }
 
@@ -19,5 +20,15 @@ function boomErrorHandler(err, req, res, next) {
   }
 }
 
+function ormErrorHandler(err,req,res,next){
+  if(err instanceof ValidationError){
+    res.status(409).json({
+      statusCode:409,
+      message:err.name,
+      errors:err.errors
+    });
+  }
+  next(err);
+}
 
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }
