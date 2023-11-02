cd1585f4356e4188e836e024b736abb7d72aea50
wagegosa <wagegosa@gmail.com>
2023-11-02 08:14:52 -0500

    Se crea configuración para conectarse a la DB

---
+++ db/config.js
@@ -0,0 +1,16 @@
+const { config } = require('./../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPass);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbname}`;
+
+module.exports = {
+    development: {
+        url: URI,
+        dialect: 'postgres',
+    },
+    production: {
+        url: URI,
+        dialect: 'postgres'
+    }
+}
