c7e79d09b2c55751e8832a4fbc159386871d16b1
wagegosa <wagegosa@gmail.com>
2023-11-02 20:28:26 -0500

    Se crea para agregar rol.

---
+++ db/migrations/20231102193031-add-role.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+const { userSchema, USER_TABLE } = require('./../models/user.model');
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.addColumn(USER_TABLE, 'role', userSchema.role);
+  },
+
+  async down (queryInterface) {
+    await queryInterface.removeColumn(USER_TABLE, 'role');
+  }
+};
